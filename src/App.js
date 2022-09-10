3a66a971dba16220e7246cede1c1213536fcc334
IgodoNdu <darkbluetechmail@gmail.com>
2022-09-10 22:56:09 +0100

    Import dependencies for routing 7 ant design
    Setting up Div structure

--- src/App.js
+++ src/App.js
@@ -1,9 +1,20 @@
 import React from "react";
+// Routing
+import { Switch, Route, Link } from 'react-router-dom';
+// Ant Design
+import { Layout, Typography, Space } from "antd";
 
 function App() {
   return (
-    <div>
-      <h1>Hello</h1>
+    <div className="app">
+      <div className="navbar">
+      </div>  
+      <div className="main">
+          <h1>Hello</h1>
+      </div>
+      <div className="footer">
+
+      </div>
     </div>
   );
 }
