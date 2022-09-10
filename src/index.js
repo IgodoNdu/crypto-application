f7801eaaf0cbe1cd0c244701890e07624b8fe121
IgodoNdu <darkbluetechmail@gmail.com>
2022-09-10 23:27:49 +0100

    Routing Implementation
    Implementing the routing on the App component

--- src/index.js
+++ src/index.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-
+// Routing
+import { BrowserRouter as Router } from 'react-router-dom';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <Router>
+      <App />
+    </Router>
   </React.StrictMode>
 );
 
