55d69594c9cd440882b44e71290b874f725351a4
IgodoNdu <darkbluetechmail@gmail.com>
2022-09-11 23:32:10 +0100

    Connect API fetch to the store
    Connecting the created API fetch to the store

--- src/app/store.js
+++ src/app/store.js
@@ -1,6 +1,11 @@
 //Here i'll create the redux store
 import { configureStore } from "@reduxjs/toolkit";
+//Connecting the Api created to the store via import
+import { cryptoApi } from "../services/cryptoApi";
 
 export default configureStore({
-    reducer: {},
+    reducer: {
+        //Now provide the reducer path
+        [cryptoApi.reducerPath]: cryptoApi.reducer,
+    },
 });
