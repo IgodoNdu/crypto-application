a4bf8363b90445e707e0ef6217170b1a23239977
IgodoNdu <darkbluetechmail@gmail.com>
2022-09-11 23:23:40 +0100

    Passed the store variable to the provider
    Passed the created store in store.js to our Provider. Then wrapped our app with the <Provider></Provider>, hence making the store content available to all the components in the App

--- src/index.js
+++ src/index.js
@@ -3,14 +3,21 @@ import ReactDOM from 'react-dom';
 import App from './App';
 //Ant Design Styling
 import 'antd/dist/antd.min.css'
+//Set the provider, (with the store as the variable)
+import { Provider } from 'react-redux';
 // Routing
 import { BrowserRouter as Router } from 'react-router-dom';
+//Import your store (i.e variable we'll provide to the provider)
+import store from './app/store';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <Router>
-      <App />
+      {/**Having got the Provider, we wrap <App /> with the provider using the store as it's prop/variable. Now every component on the app now has access to the store variable */}
+      <Provider store={store}>
+        <App />
+      </Provider>
     </Router>
   </React.StrictMode>
 );
