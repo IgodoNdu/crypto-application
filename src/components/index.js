6b0ccfc8613d71cf2132978d253fe32032be8e71
IgodoNdu <darkbluetechmail@gmail.com>
2022-09-11 11:20:12 +0100

    Update index.js to combine the created components
    Update index.js to combine the created components, and aid in streamlined import for use in APP.js

--- src/components/index.js
+++ src/components/index.js
@@ -1,2 +1,7 @@
 // sort of helps to combine the imports of multiple components
 export { default as Navbar } from "./Navbar";
+export { default as Homepage } from "./Homepage";
+export { default as Cryptocurrencies } from "./Cryptocurrencies";
+export { default as CryptoDetails } from "./CryptoDetails";
+export { default as News } from "./News";
+export { default as Exchanges } from "./Exchanges";
