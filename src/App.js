b0c25361a9c0f8e7e9554c24092873d8491e2b28
IgodoNdu <darkbluetechmail@gmail.com>
2022-09-11 14:12:46 +0100

    Footer section edit
    Include the links for the footer section of the app

--- src/App.js
+++ src/App.js
@@ -25,9 +25,17 @@ function App() {
             </Routes>
           </div>
         </Layout>
-      </div>
-      <div className="footer">
-
+        <div className="footer">
+          <Typography.Title level={5} style={{ color: 'white', textAlign: 'center' }}>
+            AmaCrypto <br />
+            All rights reserved
+          </Typography.Title>
+          <Space>
+            <Link to='/'>Home</Link>
+            <Link to={<Exchanges />}>Exchanges</Link>
+            <Link to='/news'>News</Link>
+          </Space>
+        </div>
       </div>
     </div>
   );
