39fb66f8ee14d6a79dd92e78694934dcb81444c3
IgodoNdu <darkbluetechmail@gmail.com>
2022-09-11 13:54:19 +0100

    Improve styling with Ant Design
    Improve styling with ant design distributed css

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+//Ant Design Styling
+import 'antd/dist/antd.css'
 // Routing
 import { BrowserRouter as Router } from 'react-router-dom';
 
